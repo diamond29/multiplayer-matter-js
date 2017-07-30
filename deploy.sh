f6c07312749bc63c73a5f4b0fdc0963c20d1ed76
Greg Diamond <gregdiamond29@gmail.com>
2017-07-30 15:09:02 -0400

    first attempt at game server deploy

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-echo "checkout client relase"
+echo "checkout client release"
 git checkout client/release
 echo "reset to master"
 git reset --hard master
@@ -14,3 +14,19 @@ echo "push to heroku"
 git push client-server client/release:master -f
 
 git checkout master
+
+echo "checkout server release"
+git checkout server/release
+echo "reset to master"
+git reset --hard master
+echo "copy procfile"
+cp Procfile.gameserver Procfile
+echo "commit procfile for release"
+git add Procfile
+git commit -m "server release"
+echo "push to heroku"
+git push game-server server/release:master -f
+
+git checkout master
+
+echo FINISHED
